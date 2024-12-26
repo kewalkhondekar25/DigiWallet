3b0cc415b445fd108ee349456e32f58ed4f45810
keal khondekar <kewalkhondekar25@gmail.com>
2024-12-26 16:14:07 +0530

    feat: routing added

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,19 +1,24 @@
 import './App.css'
 import { ModeToggle } from './components/theme/mode-toggle'
 import { ThemeProvider } from './components/theme/theme-provider'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import OtpPage from './pages/auth/Otp'
 import SignIn from './pages/auth/SignIn'
 import SignUp from './pages/auth/SignUp'
 
 function App() {
-  
+
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <ModeToggle/>
+      <ModeToggle />
       <div className='flex justify-center place-items-center min-h-screen'>
-        {/* <SignUp/> */}
-        {/* <SignIn/> */}
-        <OtpPage/>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/signup' element={<SignUp />} />
+            <Route path='/signin' element={<SignIn />} />
+            <Route path='/otp' element={<OtpPage />} />
+          </Routes>
+        </BrowserRouter>
       </div>
     </ThemeProvider>
   )
